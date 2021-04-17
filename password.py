f9beb914c9cc32a0d5c873648d45a3d6e079d6fa
mazzah0316 <mazzah@oregonstate.edu>
2021-04-16 22:07:43 -0700

    Final Commit

--- password.py
+++ password.py
@@ -1,3 +1,9 @@
+import random
+import string
+
 def rand_password(n):
-    password = [None] * n
+    password = ''.join((random.choice(string.ascii_letters + string.digits) for i in range(n)))
     print("Your Password of ",n, "lenght: ",password)
+
+rand_password(7)
+rand_password(3)
